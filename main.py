dd13f33664117180ec21e1648fb40152c2005199
nandansrikrishna <nandans@umich.edu>
2024-04-13 15:58:19 -0400

    Added gmbid route

--- main.py
+++ main.py
@@ -18,4 +18,27 @@ logging.basicConfig(level=logging.INFO)
 async def root():
     return {"message": "Hello, Blessed World! TYG!"}
 
+async def search_for_gmbid(term=''):
+    try:
+        search_term = f"{term}"
+        headers = {
+            'Content-Type': 'application/json',
+            'X-Goog-Api-Key': 'AIzaSyDTluSYwf0t4sD-KZE5B4UjeGHy0GaNLKw',
+            'X-Goog-FieldMask': 'places.displayName,places.id,places.formattedAddress,places.nationalPhoneNumber,places.rating,places.userRatingCount,places.websiteUri',
+        }
+
+        json_data = {
+            'textQuery': search_term,
+        }
+
+        response = requests.post('https://places.googleapis.com/v1/places:searchText', headers=headers, json=json_data)
+        return response.json().get('places',[{}])
+    except:
+        return {}
+
+@app.get("/search_gmbid/")
+async def get_places(term: str):
+    places = await search_for_gmbid(term)
+    return {"places": places}
+
 # app.mount("/", StaticFiles(directory="/app/frontend/dist"), name="static")
