c31592d739d1b1a88034111893387fb1050faa2a
nandansrikrishna <nandans@umich.edu>
2024-04-13 23:32:43 -0400

    Partial post request code: FIX_ME

--- main.py
+++ main.py
@@ -4,10 +4,13 @@ import time
 import logging
 import requests
 
+
 from fastapi import FastAPI, UploadFile, BackgroundTasks, Header
 from fastapi.responses import FileResponse
 from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
+
+from pydantic import BaseModel
 
 
 app = FastAPI()
@@ -46,6 +49,16 @@ async def get_places(term: str):
 async def floorplans():
     return [{'name': '4x4', 'unitsAvailable': '8', 'sqFt': 1302, 'pic': 'https://medialibrarycf.entrata.com/12773/MLv3/4/22/2022/3/26/15165/5dc5e18d44ced1.25555942937.jpg', 'rooms': [{'type': 'Bedroom', 'count': 4}, {'type': 'Bathroom', 'count': 4}], 'description': 'Enjoy our fully-furnished apartments equipped with smart locks, Alexa technology, in-unit laundry, and more!Schedule a tour today!'}, {'name': '5x5 Townhome', 'unitsAvailable': '7', 'sqFt': 1949, 'pic': 'https://medialibrarycf.entrata.com/12773/MLv3/4/22/2022/3/26/15163/5dcae96095f442.47368490621.jpg', 'rooms': [{'type': 'Bedroom', 'count': 5}, {'type': 'Bathroom', 'count': 5}], 'description': 'Spacious 5 private bedroom floor plan. Our largest floor plan option available in our gorgeous townhome layouts. Enjoy fully-furnished apartments equipped with smart locks, Alexa technology, in-unit laundry, and more.Schedule a tour today!'}, {'name': '3x3', 'unitsAvailable': '4', 'sqFt': 1134, 'pic': 'https://medialibrarycf.entrata.com/12773/MLv3/4/22/2022/3/26/15164/5dc5e16684e082.77139189219.jpg', 'rooms': [{'type': 'Bedroom', 'count': 3}, {'type': 'Bathroom', 'count': 3}], 'description': 'Enjoy our fully-furnished apartments equipped with smart locks, Alexa technology, in-unit laundry, and more.Schedule a tour today!'}]
 
+class Report(BaseModel):
+    type: str
+    endedReason: str
+    transcript: str
+    summary: str
+    recordingUrl: str
 
+@app.post("/report/")
+async def receive_report(report: Report):
+    print(report)
+    return {"received_data": report.dict()}
 
 # app.mount("/", StaticFiles(directory="/app/frontend/dist"), name="static")
